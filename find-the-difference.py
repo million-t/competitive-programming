4cd622946840d5501ec6a4da74ff47c3bd36b440
million-t <112546247+million-t@users.noreply.github.com>
2022-12-20 17:52:12 +0300

    Create find-the-difference.py

---
+++ find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count = {}
+        for lett in s:
+            count[lett] = count.get(lett, 0) + 1
+        for letter in t:
+            count[letter] = count.get(letter, 0) - 1
+            if count[letter] == -1:
+                return letter
+        
+        
