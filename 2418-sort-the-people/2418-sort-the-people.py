056973a7765b276d97c48d5bb7aad0c8ffae2646
million-t <112546247+million-t@users.noreply.github.com>
2023-01-17 18:25:08 +0300

    Time: 1841 ms (5.03%), Space: 14.5 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,12 +1,20 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        size = len(heights)
         
-        for i in range(len(heights)):
-            for j in range(len(heights)-i - 1):
+        for i in range(size):
+            
+            isSorted = True
+            
+            for j in range(size - i - 1):
                 
                 if heights[j] < heights[j + 1]:
+                    isSorted = False
                     
                     names[j], names[j + 1] = names[j + 1], names[j]
                     heights[j], heights[j + 1] = heights[j + 1], heights[j]
+            
+            if isSorted:
+                break
         
         return names
