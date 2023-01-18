cd03b34a786b8d4874f7d1fe960f918817e03e34
million-t <112546247+million-t@users.noreply.github.com>
2023-01-18 20:49:12 +0300

    Time: 37 ms (62.41%), Space: 13.8 MB (46.56%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        and_op_count = 0
+        
+        while n:
+            n &= (n - 1)
+            and_op_count += 1
+        
+        return and_op_count
+        
+        
