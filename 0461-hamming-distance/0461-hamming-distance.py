3fb84461f88412dd15a638f8e79b671c477d0a1d
million-t <112546247+million-t@users.noreply.github.com>
2023-02-25 21:42:14 +0300

    Time: 38 ms (24.84%), Space: 13.9 MB (49.42%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        bit_diff = x^y
+        ans = 0
+        
+        while bit_diff:
+            ans += bit_diff%2 
+            bit_diff >>= 1
+            
+        return ans
