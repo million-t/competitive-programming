5bedea12f44eb9bab8a00059f834f3a6c2f75cd1
million-t <112546247+million-t@users.noreply.github.com>
2023-02-28 11:18:24 +0300

    Time: 26 ms (93.85%), Space: 13.8 MB (93.56%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        self.computed = {0:0, 1:1} 
+    
+    def fib(self, n: int) -> int:
+        if n in self.computed: 
+            return self.computed[n]
+        
+        prev1 = self.fib(n-2)
+        prev2 = self.fib(n-1)
+        
+        self.computed[n-2] = prev1
+        self.computed[n-1] = prev2
+        self.computed[n] = prev1 + prev2
+        
+        return self.computed[n] 
+        
