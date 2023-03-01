19d2fd8a53296bd1e6f205edb9a69a08221e4125
million-t <112546247+million-t@users.noreply.github.com>
2023-03-01 12:02:44 +0300

    Time: 1670 ms (75.16%), Space: 48 MB (59.87%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -3,41 +3,25 @@ class Solution:
         n = len(nums)
         all_req = [0]*(n + 1)
         
+        
         for start, end in requests:
             all_req[start] += 1
             all_req[end + 1] -= 1
+            
         
         all_req[0] = [all_req[0], 0]
         for i in range(1, n + 1):
-            all_req[i] = [all_req[i] + all_req[i - 1][0], i]
-            
+            all_req[i] = [all_req[i] + all_req[i - 1][0], i] 
             
         
         all_req.pop()
         all_req.sort()
         nums.sort()
         
-        for i in range(n):
-            all_req[i][0] = nums[i]
-            
-            
-        for i in range(n):
-            nums[all_req[i][1]] = all_req[i][0]
-                                             
-            
-        for i in range(1, n):
-            nums[i] += nums[i - 1]
-                                             
         
         max_sum = 0
-        for start, end in requests:
-            
-            if start:
-                max_sum += nums[end] - nums[start - 1]
-            
-            else:
-                max_sum += nums[end]
-        
+        for i in range(n):
+            max_sum += nums[i]*all_req[i][0]
         
         return max_sum % (10**9 + 7)
             
