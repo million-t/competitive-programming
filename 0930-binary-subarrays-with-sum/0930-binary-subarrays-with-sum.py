9b4d703c3503b4243615af6b47a4620c231b7c03
million-t <112546247+million-t@users.noreply.github.com>
2023-03-02 21:52:13 +0300

    Time: 295 ms (81.21%), Space: 18 MB (16.04%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,15 +1,15 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
         
         seen_diff = defaultdict(int)
         subarray_count = 0
+        prefix = 0
         
-        
-        seen_diff[0] = 1
-        for prefix in nums:
+        seen_diff[prefix] = 1
+        for num in nums:
+            
+            prefix += num
             diff = prefix - goal
             
             subarray_count += seen_diff[diff]
