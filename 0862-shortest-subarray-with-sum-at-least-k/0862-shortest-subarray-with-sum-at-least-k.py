9a2844cb2a19482e97327a72b46dd47a50d767b7
million-t <112546247+million-t@users.noreply.github.com>
2023-03-02 20:10:48 +0300

    Time: 1352 ms (57.46%), Space: 28.3 MB (73.68%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -5,18 +5,22 @@ class Solution:
         for num in nums:
             prefix_sum.append(prefix_sum[-1] + num)
         
+        prefix_sum = 0
         queue = deque()
         min_len = float('inf')
         
-        for right, prefix in enumerate(prefix_sum):
-            while queue and queue[-1][0] >= prefix:
+        queue.append([0, 0])
+        for right, num in enumerate(nums):
+            prefix_sum += num
+            
+            while queue and queue[-1][0] >= prefix_sum:
                 queue.pop()
             
-            while queue and prefix - queue[0][0] >= k:
+            while queue and prefix_sum - queue[0][0] >= k:
                 less, left = queue.popleft()
-                min_len = min(min_len, right - left)
+                min_len = min(min_len, right - left + 1 ) 
             
-            queue.append([prefix, right])
+            queue.append([prefix_sum, right + 1])
         
         return min_len if min_len != float('inf') else -1
         
