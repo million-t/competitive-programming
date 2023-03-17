3fd9213127f8a471b78cc248c3f15890922b179b
million-t <112546247+million-t@users.noreply.github.com>
2023-03-17 14:20:13 +0300

    Time: 34 ms (87.02%), Space: 13.7 MB (93.13%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,6 +1,22 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return (int(math.sqrt(8*n + 1)) - 1)//2
+        
+        start = 1
+        end = n
+        
+        while start <= end:
+            
+            mid = start + (end - start)//2
+            
+            mid_sum = mid*(mid + 1)//2
+            
+            if mid_sum <= n:
+                start = mid + 1
+            
+            else:
+                end = mid - 1
+        
+        return start - 1
         
         
         
