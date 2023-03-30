7c6f4208926d388930f775848faa517a65854ff6
million-t <112546247+million-t@users.noreply.github.com>
2023-03-30 11:25:36 +0300

    Time: 53 ms (6.05%), Space: 14.2 MB (16.83%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,30 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        
         max_length = len(nums)
         permutations = []
         
+        considered = 0
+        
         def backtrack(items):
-            nonlocal nums
+            nonlocal considered
             
             if  len(items) == max_length:
                 permutations.append(items[:])
                 return
             
-            for val in nums:
-                if val not in items:
+            for index, val in enumerate(nums):
+                
+                if not considered & 1<<index:
+                    
+                    considered ^= 1<<index
                     items.append(val)
                     
                     backtrack(items)
                     
                     items.pop()
-        
+                    considered ^= 1<<index
+                    
         backtrack([])
         return permutations        
                 
