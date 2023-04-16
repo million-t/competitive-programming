e8e149c407dd558c215fef889b12ed044a47babc
million-t <112546247+million-t@users.noreply.github.com>
2023-04-16 07:59:02 +0300

    Time: 353 ms (50.24%), Space: 14.2 MB (65.06%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,16 +3,15 @@ class Solution:
         
         max_len = 0
         cur_sum = 0
-        left = 0
         
-        
-        for right in range(len(nums)):
-            cur_sum += nums[right]
+        for num in nums:
             
-            while left <= right and cur_sum != right - left + 1:
-                cur_sum -= nums[left]
-                left += 1
+            if not num:
+                cur_sum = 0
             
-            max_len = max(max_len, right - left + 1)
-        
+            else:
+                cur_sum += num
+            
+            max_len = max(max_len, cur_sum) 
+    
         return max_len
