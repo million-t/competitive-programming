e34df680bcc00379912afd102b68784ccb9a6155
million-t <112546247+million-t@users.noreply.github.com>
2023-05-08 15:36:43 +0300

    Time: 75 ms (15.00%), Space: 19.9 MB (11.80%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,19 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        
+        count = Counter(s)
+        heap = []
+        
+        for char, freq in count.items():
+            heappush(heap, (freq, char))
+        
+        res = []
+        
+        while heap:
+            freq, char = heappop(heap)
+            
+            while freq:
+                res.append(char)
+                freq -= 1
+        
+        return res[::-1]
