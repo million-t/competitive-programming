5a5fc692610b2ef849b0136dc53763d423d87608
million-t <112546247+million-t@users.noreply.github.com>
2023-05-17 17:37:01 +0300

    Time: 1954 ms (61.34%), Space: 106.1 MB (82.69%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,28 +1,49 @@
+class DSU:
+    def __init__(self, n):
+        
+        self.rep = list(range(n))
+        self.rank = [0]*n
+    
+    def find(self, x):
+        
+        root = x
+        while root != self.rep[root]:
+            root = self.rep[root]
+        
+        while x != root:
+            temp = self.rep[x]
+            self.rep[x] = root
+            x = temp
+        
+        return root
+    
+    def union(self, x, y):
+        rep_x = self.find(x)
+        rep_y = self.find(y)
+        
+        if rep_x == rep_y:
+            return
+        
+        if self.rank[rep_x] > self.rank[rep_y]:
+            self.rep[rep_y] = rep_x
+        
+        elif self.rank[rep_x] < self.rank[rep_y]:
+            self.rep[rep_x] = rep_y
+        
+        else:
+            self.rep[rep_y] = rep_x
+            self.rank[rep_x] += 1
+        
+    def is_connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         
-        graph = defaultdict(list)
+        union_find = DSU(n)
         
-        for vertex1, vertex2 in edges:
-            graph[vertex1].append(vertex2)
-            graph[vertex2].append(vertex1)
+        for u, v in edges:
+            union_find.union(u, v)
         
-        
-        visited = set()
-        
-        queue = deque([source])
-        
-        while queue:
-            
-            cur_node = queue.popleft()
-            
-            if cur_node == destination:
-                return True
-            
-            if cur_node not in visited:
-                visited.add(cur_node)
-                
-                for neighbour in graph[cur_node]:
-                    queue.append(neighbour)
-        
-        return False
+        return union_find.is_connected(source, destination)
