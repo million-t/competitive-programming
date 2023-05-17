fdcb80333c758950dd8e1240b13cf14b192d0989
million-t <112546247+million-t@users.noreply.github.com>
2023-05-17 18:47:06 +0300

    Time: 60 ms (23.67%), Space: 16.5 MB (23.67%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -32,15 +32,13 @@ class DSU:
         
         if self.rank[rep_x] >= self.rank[rep_y]:
             self.rep[rep_y] = rep_x
-            self.rank[rep_x] += self.rank[rep_y]
         
         elif self.rank[rep_x] < self.rank[rep_y]:
             self.rep[rep_x] = rep_y
-            self.rank[rep_y] += self.rank[rep_x]
         
-        # else:
-        #     self.rep[rep_y] = rep_x
-        #     self.rank[rep_x] += 1
+        else:
+            self.rep[rep_y] = rep_x
+            self.rank[rep_x] += 1
         
     def is_connected(self, x, y):
         return self.find(x) == self.find(y)
