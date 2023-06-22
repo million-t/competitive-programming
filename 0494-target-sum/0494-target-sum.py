dbb195dad7cf11fec2d3979f6844a10b34883c07
million-t <112546247+million-t@users.noreply.github.com>
2023-06-22 16:21:44 +0300

    Time: 368 ms (39.90%), Space: 43.8 MB (9.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,16 +1,23 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        @cache
+        memo = {}
+        
         def backtrack(ind, cur_target, target):
             
+            key = (ind, cur_target, target)
+            if key in memo:
+                return memo[key]
+            
             if ind >= len(nums):
-                return int(cur_target == target)
+                memo[key] = int(cur_target == target) 
+                return memo[key]
             
             neg = backtrack(ind + 1, cur_target - nums[ind], target)
             pos = backtrack(ind + 1, cur_target + nums[ind], target)
             
-            return neg + pos
+            memo[key] = neg + pos
+            return memo[key]
     
         
         return backtrack(0, 0, target)
