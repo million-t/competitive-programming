fb0b5dd6e251a2727e16a704d18e44adf272e419
million-t <112546247+million-t@users.noreply.github.com>
2023-09-05 14:30:28 +0300

    Time: 51 ms (34.67%), Space: 16.5 MB (33.63%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        if n == 1: return 1
+        
+        MOD = 10**9 + 7
+        top = [0]*(n + 1)
+        bottom = [0]*(n + 1)
+        both = [0]*(n + 1)
+        
+        top[2] = bottom[2] = 1
+        both[1] = 1
+        both[2] = 2
+        
+        for num in range(3, n + 1):
+            top[num] += both[num - 2] + bottom[num - 1]
+            bottom[num] += both[num - 2] + top[num - 1]
+            both[num] += top[num - 1] + bottom[num - 1] + both[num - 1] + both[num - 2]
+        
+        
+        
+        return both[-1]%MOD
+        
