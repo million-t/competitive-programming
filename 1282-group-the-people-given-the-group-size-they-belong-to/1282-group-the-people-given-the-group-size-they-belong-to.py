5a190d2a4fff49e58d145532e6ee1e49f161a958
million-t <112546247+million-t@users.noreply.github.com>
2023-09-11 10:09:25 +0300

    Time: 71 ms (79.24%), Space: 16.3 MB (91.52%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,26 +1,14 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         
-        temp = []
-        for ind, num in enumerate(groupSizes):
-            temp.append((num, ind))
-            
-        temp.sort()
-        length = len(temp)
-        start = end = 0
-        
+        temp = defaultdict(list)
         groups = []
-        while start < length:
+        
+        for ind, num in enumerate(groupSizes):
+            temp[num].append(ind)
             
-            inc = temp[start][0]
-            end = start + inc
-            group = [temp[start][1]]
-            start += 1
-            
-            while start < end:
-                group.append(temp[start][1])
-                start += 1
-            
-            groups.append(group)
+            if len(temp[num]) == num:
+                groups.append(temp[num])
+                temp[num] = []
         
         return groups
