9dbf90769f9985fd25562ae4f40ea282619c9c78
million-t <112546247+million-t@users.noreply.github.com>
2023-09-13 10:13:12 +0300

    Time: 155 ms (37.23%), Space: 19.1 MB (91.55%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,19 +1,23 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         
-        length = len(ratings)
-        candies = [1]*length
+        ratings.append(float('inf'))
+        prev = float('inf')
+        temp = [0]*len(ratings)
         
-        for ind in range(1, length):
+        for ind in range(len(ratings) - 1):
+            if prev >= ratings[ind] <= ratings[ind + 1]:
+                temp[ind] = 1
             
-            if ratings[ind - 1] < ratings[ind]:
-                candies[ind] = candies[ind - 1] + 1
-                
-                       
-        for ind in range(length - 2, -1, -1):
-            
-            if ratings[ind + 1] < ratings[ind]:
-                candies[ind] = max(candies[ind + 1] + 1, candies[ind])
-          
-        return sum(candies)
+            prev = ratings[ind]
         
+        for ind in range(1, len(ratings) - 1):
+            if not temp[ind] and ratings[ind - 1] < ratings[ind]:
+                temp[ind] = temp[ind - 1] + 1
+        
+        for ind in range(len(ratings) - 3, -1, -1):
+            if temp[ind] != 1 and ratings[ind + 1] < ratings[ind]:
+                temp[ind] = max(temp[ind], temp[ind + 1] + 1)
+        
+        
+        return sum(temp)
