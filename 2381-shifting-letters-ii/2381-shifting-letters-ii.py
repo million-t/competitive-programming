afed7c198106b02c0d84b41421d0db011b7163bb
million-t <112546247+million-t@users.noreply.github.com>
2023-09-14 13:07:44 +0300

    Time: 1129 ms (92.89%), Space: 41.9 MB (32.89%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,22 +1,29 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        s = list(map(lambda x: ord(x) - 97, s))
         
-        prefix_s = [0]*len(s)
+        length = len(s)
+        pref = [0]*(length + 1)
         
-        for start, end, direction in shifts:
-            prefix_s[start] += 1 if direction else -1
+        for start, end, _dir in shifts:
+            inc = 1 if _dir else -1
             
-            if end + 1 < len(s):
-                prefix_s[end + 1] += -1 if direction else 1
+            pref[start] += inc
+            pref[end + 1] -= inc
         
-        for i in range(1, len(prefix_s)):
-            prefix_s[i] += prefix_s[i-1]
+        for ind in range(1, length + 1):
+            pref[ind] += pref[ind - 1]
+        
+        ans = []
+        ord_a = ord('a')
+
+        for ind, char in enumerate(s):
+
+            ans.append(chr(ord_a + ((ord(char) - ord_a + pref[ind])%26)))
+        
+
+        return ''.join(ans)
+            
             
         
-        for i in range(len(s)):
-            s[i] = chr((s[i] + prefix_s[i])%26 + 97) 
         
-        # s = list(map(lambda x: chr(x  + 97), s))
         
-        return ''.join(s)
