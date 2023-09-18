3a69fa92ee68a422df74882b2d1eb5c9b233b954
million-t <112546247+million-t@users.noreply.github.com>
2023-09-18 11:34:10 +0300

    Time: 3615 ms (24.90%), Space: 197.9 MB (46.00%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+
+
+        memo = {}
+
+        def dp(ind):
+            length = len(stoneValue)
+            if ind >= length:
+                return 0
+                
+            if ind in memo:
+                return memo[ind]
+
+            _max = min(dp(ind + 2), dp(ind + 3), dp(ind + 4)) + stoneValue[ind]
+            if ind < length - 1:
+                _max = max(_max, min(dp(ind + 3), dp(ind + 4), dp(ind + 5)) + stoneValue[ind] + stoneValue[ind + 1])
+            
+            if ind < length - 2:
+                _max = max(_max, min(dp(ind + 4), dp(ind + 5), dp(ind + 6)) + stoneValue[ind] + stoneValue[ind + 1] + stoneValue[ind + 2])
+            
+            memo[ind] = _max
+            return _max
+        
+
+        score = dp(0)
+        _sum = sum(stoneValue)
+        if score > _sum/2:
+            return 'Alice'
+        
+        elif score < _sum/2:
+            return 'Bob'
+        
+        return 'Tie'
+
+
+
+
+
+        
+        print(score)
+        return "Alice"
+
+        
