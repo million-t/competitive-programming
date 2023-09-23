c118eee4a81c81380e951251a2a9877e380ab7e0
million-t <112546247+million-t@users.noreply.github.com>
2023-09-23 09:52:08 +0300

    Time: 213 ms (43.51%), Space: 18.5 MB (19.94%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        
+        
+        
+        words.sort(key = lambda x: len(x))
+        visited = defaultdict(int)
+        ans = 0
+        print(words)
+        
+        for word in words:
+            
+            cur_ans = 1
+            pre = [char for char in word]
+            post = []
+            
+            while pre:
+                temp = pre.pop()
+                prev = ''.join(pre + post[::-1])
+                cur_ans = max(cur_ans, visited[prev] + 1)
+                post.append(temp)
+                
+            ans = max(ans, cur_ans)
+            visited[word] = cur_ans
+                
+        return ans
+                
+        
+        
+        
