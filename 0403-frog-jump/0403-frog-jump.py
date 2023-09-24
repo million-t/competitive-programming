bda1d0b77039f7c53f401251cb844d6939312f33
million-t <112546247+million-t@users.noreply.github.com>
2023-09-24 10:35:04 +0300

    Time: 4729 ms (5.02%), Space: 22.2 MB (56.35%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        
+        
+        length = len(stones)
+        dp = defaultdict(set)
+        
+        
+        for indx in range(length - 2, -1, -1):
+            num = stones[indx]
+            dp[indx].add(stones[-1] - stones[indx])
+            for sec_indx in range(indx + 1, length - 1):
+                sec_num = stones[sec_indx]
+                k = sec_num - num
+                
+                for dif in [k - 1, k, k + 1]:
+                    if dif in dp[sec_indx]:
+                        dp[indx].add(k)
+        
+        
+        return 1 in dp[0]
+        
