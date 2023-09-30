35320347f61f389b5bed6cc418f940ddf1ccff94
million-t <112546247+million-t@users.noreply.github.com>
2023-09-30 14:14:18 +0300

    Time: 375 ms (85.55%), Space: 36.5 MB (61.36%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,25 +1,19 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         
-        stack = []
-        medium_num = float('-inf')
         
-        for i in range(len(nums) - 1, -1, -1):
-            num = nums[i]
-            
-            if num < medium_num: 
+        two = float('-inf')
+        stack = []
+        
+        for indx in range(len(nums) - 1, -1, -1):
+            num = nums[indx]
+            if num < two:
                 return True
             
             while stack and stack[-1] < num:
-                medium_num = stack.pop()
+                two = max(stack.pop(), two)
             
             stack.append(num)
             
-        return False    
-                
+        return False
         
-            
-                
-            
-                    
-                
