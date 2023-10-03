aa30158b2b74ad6cb7404c5ba0c9d3cea2dea404
million-t <112546247+million-t@users.noreply.github.com>
2023-10-03 14:33:19 +0300

    Time: 42 ms (85.07%), Space: 16 MB (99.58%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,22 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        length = len(costs)
         
-        dif_pair_map = {}
-        cost_dif = []
+        temp = [(a - b, a, b) for a, b in costs]
+        temp.sort()
+        length = len(temp)
         
-        i = 0
-        for a, b in costs:
-            dif = a - b
-            cost_dif.append((dif, i))
-            dif_pair_map[(dif, i)] = a, b
-            i += 1
-            
-        cost_dif.sort()
+        ans = [0, 0]
+        for indx in range(length):
+            city = int(indx >= length//2)
+            ans[city] += temp[indx][city + 1]
         
-        min_cost = 0
-        for index, (dif, i) in enumerate(cost_dif):    
-            min_cost += dif_pair_map[(dif, i)][index >= length//2]
-        
-        return min_cost
+        return sum(ans)
         
