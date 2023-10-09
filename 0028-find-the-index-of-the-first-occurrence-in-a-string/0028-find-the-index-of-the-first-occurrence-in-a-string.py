843d99ff81ece9f0a07826a0b8601771197444dc
million-t <112546247+million-t@users.noreply.github.com>
2023-10-09 10:38:12 +0300

    Time: 36 ms (73.47%), Space: 16.2 MB (82.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,22 +1,34 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        power = [1]
+        
+        for num in range(1, max(len(haystack), len(needle))):
+            power.append(power[-1]*27)
+        
+        target = 0
+        ord_a = ord('a')
+        for indx, char in enumerate(needle[::-1]):
+            target += (ord(char) - ord_a + 1)*power[indx]
+        
         
         win_size = len(needle)
-        size = len(haystack)
-        
-        for i in range(size):
+        left = 0
+        cur_val = 0
+
+        for right in range(len(haystack)):
+            cur_val = cur_val*27 + (ord(haystack[right]) - ord_a + 1)
             
-            if i > size - win_size:
-                break
             
-            no = False
-            for j in range(win_size):
-                if haystack[i + j] != needle[j]:
-                    no = True
-                    break
-            
-            if not no:
-                return i
+            if right - left + 1 == win_size:
+                if cur_val == target:
+                    return left
+                
+                cur_val -=  (ord(haystack[left]) - ord_a + 1)*power[win_size - 1]
+                left += 1
         
         return -1
-        
+                
+                
+            
+            
+            
