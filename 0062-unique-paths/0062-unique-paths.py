9a9bd954f03cbce735a53ddc989e64177ab278fb
million-t <112546247+million-t@users.noreply.github.com>
2023-10-12 11:23:38 +0300

    Time: 32 ms (91.16%), Space: 16.3 MB (67.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,14 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [[1]*n for _ in range(m)]
         
-        for row in range(1, m):
-            for col in range(1, n):
-                dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
+        _max = max(m - 1, n - 1)
+        _min = min(m - 1, n - 1)
         
+        ans = 1
+        spots = _max + _min
+        while spots > _max:
+            ans *= spots
+            spots -= 1
+    
         
-        return dp[m-1][n-1]
-        
-        
-        
+        return ans//factorial(_min)
