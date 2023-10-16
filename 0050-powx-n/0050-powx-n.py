df67fddfab9ff5682ade9f70cc7917ba7bd97161
million-t <112546247+million-t@users.noreply.github.com>
2023-10-16 15:05:30 +0300

    Time: 26 ms (98.98%), Space: 16.3 MB (55.12%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        def raisedTo(base,power):
-            if power == 0:
-                return 1
-            
-            elif power == 1:
-                return base
-                        
-            else:
-                temp = raisedTo(x,power//2)
-                result = base*temp*temp if power%2 else temp*temp
-                
-                return result
+        if n < 0:
+            return 1/(self.myPow(x, -n))
         
-        return raisedTo(x,abs(n)) if n>=0 else 1/(raisedTo(x,abs(n)))
-                
+        if n == 0:
+            return 1
+        
+        half = self.myPow(x, n//2)
+        
+        if n & 1:
+            return half*half*x
+        
+        return half*half
+        
+        
