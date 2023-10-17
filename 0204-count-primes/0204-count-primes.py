666b7cb85a996dc25cd8c50f82570416de83dc5b
million-t <112546247+million-t@users.noreply.github.com>
2023-10-17 11:52:18 +0300

    Time: 2562 ms (44.55%), Space: 55.1 MB (63.57%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,25 +1,26 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
         
-        if n < 2: return 0
+        if n < 3:
+            return 0
         
-        is_prime = [True]*n
-        is_prime[0] = is_prime[1] = False
+        is_prime = [1]*n
+        is_prime[0] = is_prime[1] = 0
         
+        limit = n**(0.5)
         num = 2
-        bound = n**(1/2)
         
-        while num <= bound:
+        while num <= limit:
             
-            if is_prime[num]:
-                
-                sieve = num*num
-                
-                while sieve < n:
-                    is_prime[sieve] = False
-                    sieve += num
-                
+            if not is_prime[num]:
+                num += 1
+                continue
+
+            sieve = num*num
+            while sieve < n:
+                is_prime[sieve] = 0
+                sieve += num
+            
             num += 1
-        
         
         return sum(is_prime)
