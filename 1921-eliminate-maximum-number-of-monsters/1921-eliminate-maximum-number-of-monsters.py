5454ff5829453d976c93aa6ab63068ac1c513535
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2023-11-07 20:45:53 +0300

    Time: 727 ms (27.39%), Space: 32.5 MB (19.38%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -1,14 +1,13 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        time = [d/v for d, v in zip(dist, speed)]
-        time.sort()
         
-        shot = 0
+        motion = sorted([d/v for d, v in zip(dist, speed)])
+        kills = 0
         
-        for cur_time, time_left in enumerate(time):
-            if time_left <= cur_time:
-                break
-                
-            shot += 1
+        for time, reach_time in enumerate(motion):
+            if time >= reach_time:
+                return kills
+            
+            kills += 1
         
-        return shot
+        return kills
