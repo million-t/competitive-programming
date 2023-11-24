47b2545678aec4888085dedd5c4840fe170ca5ad
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2023-11-24 12:29:21 +0300

    Time: 533 ms (41.12%), Space: 28.6 MB (51.60%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        
+        
+        piles.sort()
+        left = 0
+        right = len(piles) - 2
+        ans = 0
+        
+        while left < right:
+            ans += piles[right]    
+            right -= 2
+            left += 1
+        
+        return ans
+        
