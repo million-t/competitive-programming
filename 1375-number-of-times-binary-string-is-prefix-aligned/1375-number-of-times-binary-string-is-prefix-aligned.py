dea93fd89a8c90f0443215614b0275c7649960a1
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2023-12-22 18:45:30 +0300

    Time: 370 ms (29.12%), Space: 24.1 MB (9.34%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -1,29 +1,17 @@
 class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
         
-        pa = 0
-        before = 0
-        after = 0
-        temp = [0]*len(flips)
         
-        for indx, flip in enumerate(flips):
-            if temp[indx]:
-                after -= 1
-                before += 1
-            
-            temp[flip - 1] = 1
-            if flip - 1 <= indx:
-                before += 1
-            
-            else:
-                after += 1
-            
-            if before == indx + 1 and not after:
-                pa += 1
+        max_ = -1
+        count = ans = 0 
         
-        return pa
-                
+        for indx, num in enumerate(flips):
             
+            max_ = max(num, max_)
+            count += 1
             
+            if indx + 1 == count == max_:
+                ans += 1
         
-        
+        return ans
+            
