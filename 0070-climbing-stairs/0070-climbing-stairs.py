5bb19ba58e62befe2e46267f7a9a8e213bf08815
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-01-18 13:52:45 +0300

    Time: 45 ms (16.56%), Space: 17.3 MB (31.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.memo = {1: 1, 2: 2}
-        
     def climbStairs(self, n: int) -> int:
-        if n in self.memo:
-            return self.memo[n]
         
-        prev1 = self.climbStairs(n - 2)
-        prev2 = self.climbStairs(n - 1)
+        prev2 = 0
+        prev1 = 1
         
-        self.memo[n] = prev1 + prev2
-        return prev1 + prev2
-
+        for steps in range(2, n + 2):
+            temp = prev1
+            prev1 = prev1 + prev2
+            prev2 = temp
+        
+        return prev1
+            
+        
+        
