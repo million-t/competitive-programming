e28a954ca9904c22ba00d5b986520065b316d799
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-01-21 10:22:27 +0300

    Time: 43 ms (30.58%), Space: 16.6 MB (56.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,13 +5,13 @@ class Solution:
         if size < 2:
             return max(nums)
         
-        memo = [0]*size
-        memo[0] = nums[0]
-        memo[1] = max(nums[0], nums[1])
+        dp = [0]*size
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
         
         for ind in range(2, size):
-            memo[ind] = max(memo[ind - 1], memo[ind - 2] + nums[ind])
+            dp[ind] = max(dp[ind - 1], dp[ind - 2] + nums[ind])
         
-        return max(memo[-1], memo[-2])
+        return max(dp[-1], dp[-2])
                 
         
