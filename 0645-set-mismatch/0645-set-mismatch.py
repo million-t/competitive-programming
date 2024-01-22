b5b346aab398b4779cce78d2328e8d2e960c24cf
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-01-22 11:06:38 +0300

    Time: 164 ms (70.78%), Space: 18 MB (86.03%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,11 +1,11 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        
         length = len(nums)
         index = 0
         
         
-        while index < length:
-            
+        while index < length:    
             target_ind = nums[index] - 1
             
             if target_ind != index and nums[target_ind] != nums[index]:
