5ec93bd54ba811eead666ee578283b03c333ef8f
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-02-08 20:37:50 +0300

    Time: 98 ms (75.79%), Space: 17.5 MB (82.50%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,24 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        
+        
+        size = len(customers)
+        sufy = customers.count('Y')
+        prefn = 0
+        penalty = sufy
+        ans = 0
+        
+        for indx, char in enumerate(customers):
+            
+            if char == 'Y':
+                sufy -= 1
+            
+            else:
+                prefn += 1
+            
+            if sufy + prefn < penalty:
+                ans = indx + 1
+                penalty = sufy + prefn
+                
+        return ans
+        
