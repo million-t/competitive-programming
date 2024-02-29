1a38984f82d93242d8f2e7b9b418ac4651aaaa9c
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-02-29 13:27:14 +0300

    Time: 588 ms (47.97%), Space: 29.4 MB (82.61%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -2,10 +2,10 @@ class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         
         
-        ans = nums[0]
-        pref = nums[0]
+        ans = 0
+        pref = 0
         
-        for indx in range(1, len(nums)):
+        for indx in range(len(nums)):
             pref += nums[indx]
             ans = max(ans, ceil(pref/(indx + 1)))
         
