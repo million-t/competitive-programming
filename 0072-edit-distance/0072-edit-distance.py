9ff22cf84f87ffad846877b96284cf9bddee88f9
Million Tolessa Sedi <112546247+million-t@users.noreply.github.com>
2024-07-17 22:09:27 +0300

    Update 0072-edit-distance.py

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,22 +2,23 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
         @cache
-        def dp(indx1, indx2):
+        def dp(index1, index2):
+            if index2 < 0:
+                return index1 + 1
+            elif index1 < 0:
+                return index2 + 1
             
-            if indx2 < 0:
-                return indx1 + 1
-            
-            elif indx1 < 0:
-                return indx2 + 1
-            
-            if word1[indx1] == word2[indx2]:
-                return dp(indx1 - 1, indx2 - 1)
+            # characters already match
+            if word1[index1] == word2[index2]:
+                return dp(index1 - 1, index2 - 1)
             
             else:
-                ans1 = dp(indx1, indx2 - 1) + 1
-                ans2 = dp(indx1 - 1, indx2) + 1
-                ans3 = dp(indx1 - 1, indx2 - 1) + 1
+                # insert a character
+                ans1 = dp(index1, index2 - 1) + 1
+                # delete a character
+                ans2 = dp(index1 - 1, index2) + 1
+                # replace a character
+                ans3 = dp(index1 - 1, index2 - 1) + 1
                 return min(ans1, ans2, ans3)
-            
             
         return dp(len(word1) - 1, len(word2) - 1)
